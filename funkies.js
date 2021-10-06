c7bf6dfce3cc3aab4a721fccadf7082c07c891fd
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-06 13:37:32 -0700

    added all html elements and added minimal styling

---
+++ funkies.js
@@ -0,0 +1,10 @@
+export function didUserWin(userChoice, actualFlip) {
+    if userChoice === actualFlip) {
+        return true;
+    } else { return false};
+
+
+    //return userChoice === actualFlip;
+}
+
+//
