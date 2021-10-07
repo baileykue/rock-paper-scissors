70c7c21a6a809468928fc4fb819ea19f994b5fe4
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-06 18:54:58 -0700

    added a new test

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,15 @@
-/*IMPORT MODULES under test here:
+
 import { didUserWin } from '../funkies';
 
 const test = QUnit.test;
 
 test('user: rock, computer: paper', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const userChoice = -1;
+  
+    const expected = -1;
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actualChoice = -1;
+    const actual = didUserWin('rock', 'scissors');
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(-1, -1);
-});*/
+    expect.equal(actual, expected);
+});
+
+
